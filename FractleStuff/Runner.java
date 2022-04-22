a34054e04d0a41b0927aefd26f92417788d8d4ba
sperezgomez04 <sperezgomez@stu.naperville203.org>
2022-04-22 12:22:30 -0500

    triangl4es

--- FractleStuff/Runner.java
+++ FractleStuff/Runner.java
@@ -2,7 +2,7 @@ public class Runner
 {
     public static void main(String[] args)
     {
-        CircleViewer viewer = new CircleViewer();
+        //CircleViewer viewer = new CircleViewer();
         
         SierpinskiViewer viewer2 = new SierpinskiViewer();
 
--- FractleStuff/SierpinskiGasket.java
+++ FractleStuff/SierpinskiGasket.java
@@ -1,3 +1,5 @@
+
+
 import java.awt.*;
 import javax.swing.JPanel;
 import java.awt.geom.Line2D;
@@ -69,21 +71,62 @@ public class SierpinskiGasket extends JPanel
        int dxsquared = (int)Math.pow(xPos[0] - xPos[1],2);
        int dysquared = (int)Math.pow(yPos[0] - yPos[1],2);
        int distance = (int)Math.sqrt(dxsquared + dysquared);
-
-
+       int ABMidx = ((xPos[0]+xPos[1])/2); 
+        int ABMidy = ((yPos[0]+yPos[1])/2);
+        int BCMidx = ((xPos[1]+xPos[2])/2); 
+        int BCMidy = ((yPos[1]+yPos[2])/2);
+        int CAMidx = ((xPos[2]+xPos[3])/2); 
+        int CAMidy = ((yPos[2]+yPos[3])/2);
+        int[] xPos1 = {ABMidx, BCMidx,CAMidx, ABMidx}; 
+        int[] yPos1 = {ABMidy, BCMidy,CAMidy, ABMidy}; 
+       
+       page.setColor (Color.yellow);
+       page.drawPolyline (xPos1, yPos1, xPos1.length);
+       
 
         if (distance > 10)
         {
             //find midpoints and draw triangle
-            int ABMidx = ((xPos[0]+xPos[1])/2)
-            int ABMidy = (
+            int ABMidx2 = ((xPos1[0]+xPos1[1])/2); 
+            int ABMidy2 = ((yPos1[0]+yPos1[1])/2);
+            int BCMidx2 = ((xPos1[1]+xPos1[2])/2);
+            int BCMidy2 = ((yPos1[1]+yPos1[2])/2);
+            int CAMidx2 = ((xPos1[2]+xPos1[3])/2);
+            int CAMidy2 = ((yPos1[2]+yPos1[3])/2);
             
+            int topx = ((xPos[2]+BCMidx)/2); 
+            int topy = ((yPos[2]+BCMidy)/2);
+            int top2x = ((xPos[2]+CAMidx)/2); 
+            int top2y = ((yPos[2]+CAMidy)/2);
+            
+            int leftx = ((xPos[1]+BCMidx)/2); 
+            int lefty = ((yPos[1]+BCMidy)/2); 
+            int left2x = ((xPos[1]+ABMidx)/2); 
+            int left2y = ((yPos[1]+ABMidy)/2);
+            
+            int rightx = ((xPos[0]+ABMidx)/2); 
+            int righty = ((yPos[0]+ABMidy)/2);
+            int right2x = ((xPos[0]+CAMidx)/2); 
+            int right2y = ((yPos[0]+CAMidy)/2);
+           
+            
+            int[] topX = {topx, top2x, ABMidx2, topx}; 
+            int[] topY = {topy, top2y, ABMidy2, topy}; 
+            int[] leftX = {leftx, left2x, BCMidx2, leftx}; 
+            int[] leftY = {lefty, left2y, BCMidy2, lefty}; 
+            int[] rightX = {rightx, right2x, CAMidx2, rightx}; 
+            int[] rightY = {righty, right2y, CAMidy2, righty}; 
             //draw the tri
-            
+            page.setColor (Color.yellow);
+            page.drawPolyline (topX, topY, topX.length);
+            page.drawPolyline (leftX, leftY, leftX.length);
+            page.drawPolyline (rightX, rightY, rightX.length);
             
             
             //call the 3 new triangles
-           
+            Triangle(topX, topY,page);
+            Triangle(leftX, leftY,page);
+            Triangle(rightX, rightY,page);
 
 
         }
